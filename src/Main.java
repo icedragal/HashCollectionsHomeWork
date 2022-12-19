d9a0440d51211b26d8e14cc122f751b4ac1174e0
Icedragal <icedragal@gmail.com>
2022-12-19 20:32:37 +0300

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        Product banana = new Product("Банан", 110, 1);
+        Product orange = new Product("Апельсин", 150, 5);
+        Product apple = new Product("Яблоко", 50, 1);
+        ProductList productList = new ProductList();
+        productList.add(banana);
+        productList.add(orange);
+        productList.add(apple);
     }
 }
