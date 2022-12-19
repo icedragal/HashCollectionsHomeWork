5b24ec1c68beaa16bccbbd0d9ed22b8437dc7ee1
Icedragal <icedragal@gmail.com>
2022-12-19 20:44:48 +0300

    Задание 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Product banana = new Product("Банан", 110, 1);
@@ -7,5 +12,23 @@ public class Main {
         productList.add(banana);
         productList.add(orange);
         productList.add(apple);
+
+        Random random = new Random();
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size()<20){
+            numbers.add(random.nextInt(1001));
+        }
+        System.out.println(numbers);
+
+        Iterator<Integer> iterator = numbers.iterator();
+
+        while (iterator.hasNext()){
+            Integer number = iterator.next();
+            if (number % 2 == 1){
+                iterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
